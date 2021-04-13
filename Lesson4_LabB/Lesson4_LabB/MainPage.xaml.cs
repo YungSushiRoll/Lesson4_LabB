d371817f5bfb8fb98ae844d26e972b36d2236d19
YungSushiRoll <j.gosset1@gmail.com>
2021-04-12 23:57:32 -0500

    Add project files.

---
+++ Lesson4_LabB/Lesson4_LabB/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson4_LabB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ToNextPage(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MeterPage());
+        }
+    }
+}
---
+++ Lesson4_LabB/Lesson4_LabB/MeterPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lesson4_LabB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MeterPage : ContentPage
+    {
+        public MeterPage()
+        {
+            InitializeComponent();
+        }
+
+        private string journal = "";
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            journal = theWhy.Text;
+            theWhy.Text = "";
+            theJournal.Text = journal;
+        }
+    }
+}
